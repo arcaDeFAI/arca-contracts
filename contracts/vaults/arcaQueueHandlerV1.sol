c1fce019e4587d0d3ee731a6b6396f7ffe4c5490
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-06-13 11:20:59 -0400

    Use queue slices

--- contracts/vaults/arcaQueueHandlerV1.sol
+++ contracts/vaults/arcaQueueHandlerV1.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// This interface is just so to share some struct definitions between contracts
 interface IDepositWithdrawCompatible {
     // Structs for queue management
     struct DepositRequest {
@@ -22,8 +23,8 @@ interface IDepositWithdrawCompatible {
 
 contract arcaQueueHandlerV1 is Ownable, IDepositWithdrawCompatible {
     // Queue management
-    DepositRequest[] public depositQueue;
-    WithdrawRequest[] public withdrawQueue;
+    DepositRequest[] private depositQueue;
+    WithdrawRequest[] private withdrawQueue;
     uint256 public depositQueueStart;
     uint256 public withdrawQueueStart;
     
@@ -38,6 +39,48 @@ contract arcaQueueHandlerV1 is Ownable, IDepositWithdrawCompatible {
     constructor() Ownable(msg.sender) {
         depositQueueStart = 0;
         withdrawQueueStart = 0;
+    }
+
+    function reduceQueuedToken(uint256 amount, bool isTokenX) external onlyOwner {
+        if (isTokenX) {
+            require(queuedTokenX >= amount, "Cannot remove more tokens than queued");
+            queuedTokenX -= amount;
+        } else {
+            require(queuedTokenY >= amount, "Cannot remove more tokens than queued");
+            queuedTokenY -= amount;
+        }
+    }
+
+    function getDepositQueueTrailingSlice() external onlyOwner returns(DepositRequest[] memory){
+        uint length = depositQueue.length;
+        require(depositQueueStart <= length, "Deposit queue start out of bounds");
+        uint sliceLength = length - depositQueueStart;
+        DepositRequest[] memory slicedDepositQueue = new DepositRequest[](sliceLength);
+
+        for (uint i = 0; i < sliceLength; i++) {
+            slicedDepositQueue[i] = depositQueue[i + depositQueueStart];
+        }
+
+        // Clear processed deposits
+        depositQueueStart += sliceLength;
+
+        return slicedDepositQueue;
+    }
+
+    function getWithdrawQueueTrailingSlice() external onlyOwner returns(WithdrawRequest[] memory){
+        uint length = withdrawQueue.length;
+        require(withdrawQueueStart <= length, "Withdraw queue start out of bounds");
+        uint sliceLength = length - withdrawQueueStart;
+        WithdrawRequest[] memory slicedWithdrawQueue = new WithdrawRequest[](sliceLength);
+
+        for (uint i = 0; i < sliceLength; i++) {
+            slicedWithdrawQueue[i] = withdrawQueue[i + withdrawQueueStart];
+        }
+
+        // Clear processed withdrawals
+        withdrawQueueStart += sliceLength;
+
+        return slicedWithdrawQueue;
     }
 
     function enqueueDepositRequest(DepositRequest memory depositRequest) external onlyOwner {
@@ -60,7 +103,7 @@ contract arcaQueueHandlerV1 is Ownable, IDepositWithdrawCompatible {
         emit WithdrawQueued(msg.sender, withdrawRequest.sharesX, withdrawRequest.sharesY);
     }
 
-        // View functions for queue management
+    // View functions for queue management
     /**
      * @dev Get deposit queue length
      */
@@ -87,156 +130,5 @@ contract arcaQueueHandlerV1 is Ownable, IDepositWithdrawCompatible {
      */
     function getPendingWithdrawsCount() external view returns (uint256) {
         return withdrawQueue.length - withdrawQueueStart;
-    }
-
-    /**
-     * @dev Internal function to process withdraw queue
-     * Calculates each user's share of withdrawn tokens and processes their withdrawal
-     */
-    function processWithdrawQueue(uint256 totalXRemoved, uint256 totalYRemoved) private onlyOwner returns (uint256 processed) {
-        uint256 queueLength = withdrawQueue.length;
-        
-        for (uint256 i = withdrawQueueStart; i < queueLength; i++) {
-            WithdrawRequest memory request = withdrawQueue[i];
-            
-            // Calculate user's share of withdrawn tokens
-            uint256 userAmountX = 0;
-            uint256 userAmountY = 0;
-            
-            if (request.sharesX > 0 && totalSharesX > 0) {
-                // Share of removed liquidity
-                userAmountX = (totalXRemoved * request.sharesX) / totalSharesX;
-                uint256 existingX = balanceX();
-                if (existingX > 0) {
-                    userAmountX += (existingX * request.sharesX) / totalSharesX;
-                }
-            }
-            
-            if (request.sharesY > 0 && totalSharesY > 0) {
-                // Share of removed liquidity
-                userAmountY = (totalYRemoved * request.sharesY) / totalSharesY;
-                uint256 existingY = balanceY();
-                if (existingY > 0) {
-                    userAmountY += (existingY * request.sharesY) / totalSharesY;
-                }
-            }
-            
-            // Calculate withdraw fee on total withdrawal amount
-            uint256 totalWithdrawAmount = userAmountX + userAmountY;
-            uint256 withdrawFee = (totalWithdrawAmount * feeManager.getWithdrawFee()) / feeManager.BASIS_POINTS();
-            
-            // Apply fee proportionally to both tokens
-            if (withdrawFee > 0 && totalWithdrawAmount > 0) {
-                uint256 feeX = (userAmountX * withdrawFee) / totalWithdrawAmount;
-                uint256 feeY = (userAmountY * withdrawFee) / totalWithdrawAmount;
-                
-                userAmountX -= feeX;
-                userAmountY -= feeY;
-                
-                // Send fees to fee recipient
-                if (feeX > 0) {
-                    IERC20(vaultConfig.tokenX).safeTransfer(feeManager.getFeeRecipient(), feeX);
-                }
-                if (feeY > 0) {
-                    IERC20(vaultConfig.tokenY).safeTransfer(feeManager.getFeeRecipient(), feeY);
-                }
-                
-                emit FeeCollected(feeManager.getFeeRecipient(), withdrawFee, "withdraw");
-            }
-            
-            // Burn user's shares
-            sharesX[request.user] -= request.sharesX;
-            sharesY[request.user] -= request.sharesY;
-            totalSharesX -= request.sharesX;
-            totalSharesY -= request.sharesY;
-            
-            // Transfer tokens to user
-            if (userAmountX > 0) {
-                IERC20(vaultConfig.tokenX).safeTransfer(request.user, userAmountX);
-            }
-            if (userAmountY > 0) {
-                IERC20(vaultConfig.tokenY).safeTransfer(request.user, userAmountY);
-            }
-            
-            emit WithdrawProcessed(request.user, userAmountX, userAmountY);
-            processed++;
-        }
-        
-        // Clear processed withdrawals
-        if (processed > 0) {
-            withdrawQueueStart = queueLength;
-        }
-        
-        return processed;
-    }
-
-    // TODO RAPH: I want to move the processDepositQueue and processWithdraw thing to the queueHandler
-    // For this to be possible, I will need to pass a lot of arguments, modifying arrays "in place" (by reference)
-    // https://ethereum.stackexchange.com/questions/98855/modify-in-place-an-array-received-as-function-argument
-    // I should possibly use a struct for the input to group everything together
-
-    /**
-     * @dev Internal function to process deposit queue
-     * Mints shares based on current token balances after withdrawals are processed and rewards compounded
-     */
-    function processDepositQueue() private onlyOwner returns (uint256 processed) {
-        uint256 queueLength = depositQueue.length;
-            
-        for (uint256 i = depositQueueStart; i < queueLength; i++) {
-            DepositRequest memory request = depositQueue[i];
-            
-            uint256 newShares = 0;
-            
-            if (request.isTokenX) {
-                // Calculate sharesX to mint (benefits from compounded rewards increasing balance)
-                if (totalSharesX == 0) {
-                    newShares = request.amount;
-                } else {
-                    uint256 currentBalanceX = balanceX();
-                    if (currentBalanceX > 0) {
-                        newShares = (request.amount * totalSharesX) / currentBalanceX;
-                    } else {
-                        newShares = request.amount; // Fallback if no balance
-                    }
-                }
-                
-                // Update user shares and totals AFTER calculation
-                sharesX[request.user] += newShares;
-                totalSharesX += newShares;
-                
-                // Remove from queued tokens (this adds to available balance for next person)
-                queuedTokenX -= request.amount;
-                
-            } else {
-                // Calculate sharesY to mint (benefits from compounded rewards increasing balance)
-                if (totalSharesY == 0) {
-                    newShares = request.amount;
-                } else {
-                    uint256 currentBalanceY = balanceY();
-                    if (currentBalanceY > 0) {
-                        newShares = (request.amount * totalSharesY) / currentBalanceY;
-                    } else {
-                        newShares = request.amount; // Fallback if no balance
-                    }
-                }
-                
-                // Update user shares and totals AFTER calculation
-                sharesY[request.user] += newShares;
-                totalSharesY += newShares;
-                
-                // Remove from queued tokens (this adds to available balance for next person)
-                queuedTokenY -= request.amount;
-            }
-            
-            emit SharesMinted(request.user, request.isTokenX ? newShares : 0, request.isTokenX ? 0 : newShares);
-            processed++;
-        }
-        
-        // Clear processed deposits
-        if (processed > 0) {
-            depositQueueStart = queueLength;
-        }
-        
-        return processed;
     }
 }
