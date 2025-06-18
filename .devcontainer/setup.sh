8d1e689fce128efd9377386911dc6a7af99b6b8c
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-06-17 20:19:26 -0400

    Added npm run autocomplete to bash
    It's really cool actually, let's you easily & naturally discover the commands we added

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,5 +2,6 @@
 echo "Dev container custom setup..!"
 echo "alias c='clear'" >> ~/.bash_aliases
 echo "alias ll='ls -Alh'" >> ~/.bash_aliases
+echo "source <(npm completion)" >> ~/.bash_aliases
 
 npm ci
