1362c90c8c5665a6b4ba3979bdcc3a869300e5c5
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-06-20 13:16:57 -0400

    Add view queue function for tests, fix bugs

--- contracts/mocks/MockLBHooksBaseRewarder.sol
+++ contracts/mocks/MockLBHooksBaseRewarder.sol
@@ -6,6 +6,7 @@ import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 contract MockLBHooksBaseRewarder {
     IERC20 public rewardToken;
     uint256 private claimAmount;
+    bool private shouldFail;
 
     constructor() {}
 
@@ -17,7 +18,15 @@ contract MockLBHooksBaseRewarder {
         claimAmount = _amount;
     }
 
+    function setShouldFail(bool _shouldFail) external {
+        shouldFail = _shouldFail;
+    }
+
     function claim(address user, uint256[] calldata /* binIds */) external {
+        if (shouldFail) {
+            revert("Mock claim failure");
+        }
+        
         if (claimAmount > 0 && address(rewardToken) != address(0)) {
             // Transfer reward tokens to the claimer
             rewardToken.transfer(user, claimAmount);
