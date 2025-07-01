d416e051a487a4751ff6a7c9f74641613c6380ea
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-06-30 22:15:42 -0400

    WIP adding coming soon website
    Signed-off-by: a-crypto-bro <a.crypto.bro1@gmail.com>

---
+++ arca-coming-soon/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <img src="/coming-soon.jpg" alt="Coming Soon Arca Finance" className="hero-image" />
+      <h1>Coming Soon</h1>
+      <p>Arca Finance - Stay tuned for our DeFi launch.</p>
+    </div>
+  );
+}
+
+export default App;
