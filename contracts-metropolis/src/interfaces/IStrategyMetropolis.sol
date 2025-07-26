cc4ba70d7b73e831f97e9ed5ea57aedb9c80ddbd
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-07-26 15:26:51 -0400

    Create strategy interfaces

---
+++ contracts-metropolis/src/interfaces/IStrategyMetropolis.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {ILBPair} from "joe-v2/interfaces/ILBPair.sol";
+import {IStrategyCommon} from "./IStrategyCommon.sol";
+
+/**
+ * @title Metropolis Strategy Interface
+ * @author Arca
+ * @notice Interface for Metropolis-specific strategy functionality
+ */
+interface IStrategyMetropolis is IStrategyCommon {
+    // Metropolis-specific errors
+    error Strategy__InvalidRange();
+    error Strategy__ActiveIdSlippage();
+    error Strategy__RangeAlreadySet();
+    error Strategy__RangeTooWide();
+    error Strategy__InvalidLength();
+
+    // Metropolis-specific events
+    event RangeSet(int32 low, int32 upper);
+
+    // Metropolis-specific getters
+    function getPair() external pure returns (ILBPair);
+    function getRange() external view returns (int32 low, int32 upper);
+    function getMaxRange() external view returns (uint256);
+
+    // Metropolis-specific rebalance
+    function rebalance(
+        int32 newLower,
+        int32 newUpper,
+        int32 desiredActiveId,
+        int32 slippageActiveId,
+        uint256 amountX,
+        uint256 amountY,
+        bytes calldata distributions
+    ) external;
+}
