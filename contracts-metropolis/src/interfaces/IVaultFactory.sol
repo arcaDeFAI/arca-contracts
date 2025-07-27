14303fee4e5aac23a52e01a46717e80e125be4f6
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-07-26 23:25:09 -0400

    Trim VaultFactory to save contract size space...
    Also apply some linting

--- contracts-metropolis/src/interfaces/IVaultFactory.sol
+++ contracts-metropolis/src/interfaces/IVaultFactory.sol
@@ -2,12 +2,13 @@
 
 pragma solidity 0.8.26;
 
-import {IERC20Upgradeable} from "openzeppelin-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {
+    IERC20Upgradeable
+} from "openzeppelin-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {ILBPair} from "joe-v2/interfaces/ILBPair.sol";
 
 import {IAggregatorV3} from "./IAggregatorV3.sol";
 import {IStrategyCommon} from "./IStrategyCommon.sol";
-import {IBaseVault} from "./IBaseVault.sol";
 import {IMinimalVault} from "./IMinimalVault.sol";
 import {IOracleHelper} from "./IOracleHelper.sol";
 
@@ -79,11 +80,20 @@ interface IVaultFactory {
 
     event TransferIgnoreListSet(address[] addresses);
 
-    event VaultImplementationSet(VaultType indexed vType, address indexed vaultImplementation);
+    event VaultImplementationSet(
+        VaultType indexed vType,
+        address indexed vaultImplementation
+    );
 
-    event StrategyImplementationSet(StrategyType indexed sType, address indexed strategyImplementation);
+    event StrategyImplementationSet(
+        StrategyType indexed sType,
+        address indexed strategyImplementation
+    );
 
-    event DefaultOperatorSet(address indexed sender, address indexed defaultOperator);
+    event DefaultOperatorSet(
+        address indexed sender,
+        address indexed defaultOperator
+    );
 
     event FeeRecipientSet(address indexed sender, address indexed feeRecipient);
 
@@ -98,27 +108,54 @@ interface IVaultFactory {
 
     function getWNative() external view returns (address);
 
-    function getVaultAt(VaultType vType, uint256 index) external view returns (address);
+    function getVaultAt(
+        VaultType vType,
+        uint256 index
+    ) external view returns (address);
+
+    function createMarketMakerShadowOracleRewardVault(
+        address pool,
+        uint16 aumFee,
+        uint32 twapInterval
+    ) external payable returns (address vault, address strategy);
+
+    function createMarketMakerOracleVault(
+        ILBPair lbPair,
+        uint16 aumFee
+    ) external payable returns (address vault, address strategy);
 
     function getVaultType(address vault) external view returns (VaultType);
 
-    function getStrategyAt(StrategyType sType, uint256 index) external view returns (address);
+    function getStrategyAt(
+        StrategyType sType,
+        uint256 index
+    ) external view returns (address);
 
-    function getStrategyType(address strategy) external view returns (StrategyType);
+    function getStrategyType(
+        address strategy
+    ) external view returns (StrategyType);
 
     function getNumberOfVaults(VaultType vType) external view returns (uint256);
 
-    function getNumberOfStrategies(StrategyType sType) external view returns (uint256);
+    function getNumberOfStrategies(
+        StrategyType sType
+    ) external view returns (uint256);
 
     function getDefaultOperator() external view returns (address);
 
     function getFeeRecipient() external view returns (address);
 
-    function getFeeRecipientByVault(address vault) external view returns (address);
+    function getFeeRecipientByVault(
+        address vault
+    ) external view returns (address);
 
-    function getVaultImplementation(VaultType vType) external view returns (address);
+    function getVaultImplementation(
+        VaultType vType
+    ) external view returns (address);
 
-    function getStrategyImplementation(StrategyType sType) external view returns (address);
+    function getStrategyImplementation(
+        StrategyType sType
+    ) external view returns (address);
 
     function getDepositToWithdrawCooldown() external view returns (uint256);
 
@@ -128,59 +165,78 @@ interface IVaultFactory {
 
     function isPairWhitelisted(address pair) external view returns (bool);
 
-    function setVaultImplementation(VaultType vType, address vaultImplementation) external;
+    function setVaultImplementation(
+        VaultType vType,
+        address vaultImplementation
+    ) external;
 
-    function setStrategyImplementation(StrategyType sType, address strategyImplementation) external;
+    function setStrategyImplementation(
+        StrategyType sType,
+        address strategyImplementation
+    ) external;
 
     function setDefaultOperator(address defaultOperator) external;
 
     function setOperator(IStrategyCommon strategy, address operator) external;
 
-    function setPendingAumAnnualFee(IMinimalVault vault, uint16 pendingAumAnnualFee) external;
+    function setPendingAumAnnualFee(
+        IMinimalVault vault,
+        uint16 pendingAumAnnualFee
+    ) external;
 
     function resetPendingAumAnnualFee(IMinimalVault vault) external;
 
     function setFeeRecipient(address feeRecipient) external;
 
-    function setDefaultSequencerUptimeFeed(IAggregatorV3 sequencerUptimeFeed) external;
+    function setDefaultSequencerUptimeFeed(
+        IAggregatorV3 sequencerUptimeFeed
+    ) external;
 
-    function setSequenzerUptimeFeed(address oracleVault, IAggregatorV3 sequencerUptimeFeed) external;
+    function setSequencerUptimeFeed(
+        address oracleVault,
+        IAggregatorV3 sequencerUptimeFeed
+    ) external;
 
-    function setOracleParameters(address oracleVault, IOracleHelper.OracleParameters calldata parameters) external;
+    function setOracleParameters(
+        address oracleVault,
+        IOracleHelper.OracleParameters calldata parameters
+    ) external;
 
-    function setPairWhitelist(address[] calldata pairs, bool isWhitelisted) external;
+    function setPairWhitelist(
+        address[] calldata pairs,
+        bool isWhitelisted
+    ) external;
 
-    function createOracleVaultAndDefaultStrategy(ILBPair lbPair, IAggregatorV3 dataFeedX, IAggregatorV3 dataFeedY, uint24 heartbeatX, uint24 heartbeatY)
-        external
-        returns (address vault, address strategy);
-
-    function createOracleVault(ILBPair lbPair, IAggregatorV3 dataFeedX, IAggregatorV3 dataFeedY, uint24 heartbeatX, uint24 heartbeatY)
-        external
-        returns (address vault);
-
-    function createDefaultStrategy(IBaseVault vault) external returns (address strategy);
-
-    function createShadowOracleRewardVaultAndStrategy(
-        address pool
-    ) external returns (address vault, address strategy);
-
-    function linkVaultToStrategy(IMinimalVault vault, address strategy) external;
+    function linkVaultToStrategy(
+        IMinimalVault vault,
+        address strategy
+    ) external;
 
     function setEmergencyMode(IMinimalVault vault) external;
 
     function cancelShutdown(address oracleVault) external;
 
-    function recoverERC20(IMinimalVault vault, IERC20Upgradeable token, address recipient, uint256 amount) external;
-    
+    function recoverERC20(
+        IMinimalVault vault,
+        IERC20Upgradeable token,
+        address recipient,
+        uint256 amount
+    ) external;
+
     function setTransferIgnoreList(address[] calldata addresses) external;
 
     function setRebalanceCoolDown(address strategy, uint256 coolDown) external;
 
-    function getShadowNonfungiblePositionManager() external view returns (address);
+    function getShadowNonfungiblePositionManager()
+        external
+        view
+        returns (address);
 
     function getShadowVoter() external view returns (address);
 
-    function setShadowNonfungiblePositionManager(address nonfungiblePositionManager) external;
+    function setShadowNonfungiblePositionManager(
+        address nonfungiblePositionManager
+    ) external;
 
     function setShadowVoter(address voter) external;
 }
