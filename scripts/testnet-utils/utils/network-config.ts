75beff11ca686777d820ed805a738e6e1ef9a753
a-crypto-bro <a.crypto.bro1@gmail.com>
2025-08-10 21:20:22 -0400

    Massive clean up 2
    Delete script archives, move docs to docs folder, delete remappings-metropolis.txt

--- scripts/testnet-utils/utils/network-config.ts
+++ scripts/testnet-utils/utils/network-config.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import type { DeploymentConfig } from "../deployArcaSystem";
+import type { DeploymentConfig } from "../../archive/deployArcaSystem";
 
 export interface NetworkConfig {
   name: string;
